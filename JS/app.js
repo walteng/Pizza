b9223534435cad5df88ea1e8c113661e66fa0458
walteng <ngetichwalt49@gmail.com>
2019-06-17 11:49:07 +0300

    added inline script and deleted code in js file

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-  $(".delv").click(function(){
-    $(".input").toggle();
-  });
-});
-function myFunction() {
-  var x = document.getElementById("size").selectedIndex;
-  var y = document.getElementById("crust").selectedIndex;
-  var z = document.getElementById("topping").selectedIndex;
-  parseFloat(document.getElementById("price").value)= (parseFloat(document.getElementsByTagName("option")[x].value)+ parseFloat(document.getElementsByTagName("option")[y].value) + parseFloat(document.getElementsByTagName("option")[z].value ));
-}
+
