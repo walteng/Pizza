b8743a6d9977d2405c24c91c03e88559b0b30d9b
walteng <ngetichwalt49@gmail.com>
2019-06-17 14:40:19 +0300

    changed files to add output element and added proper javascript code

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,14 @@
+// BI LOGIC
 
+
+function getPizza() {
+  var p = document.getElementById("pa");
+  var x = document.getElementById("size").selectedIndex;
+  var y = document.getElementById("crust").selectedIndex;
+  var z = document.getElementById("topping").selectedIndex;
+  p.innerHTML = parseFloat(document.getElementsByTagName("option")[x].value) + parseFloat(document.getElementsByTagName("option")[y].value) + parseFloat(document.getElementsByTagName("option")[z].value)
+}
+
+
+
+// UI LOGIC
